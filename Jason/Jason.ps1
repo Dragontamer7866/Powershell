c247fbb1e1b63dfc28967bf00e7d8f0278cb150f
Dragontamer7866 <tacoboycharles@gmail.com>
2025-05-19 18:21:46 -0500

    Update Jason.ps1

--- Jason/Jason.ps1
+++ Jason/Jason.ps1
@@ -1,5 +1,8 @@
 Clear-Host -Force
 @echo off
 #Requires -RunAsAdministrator -Force
+Get-ExecutionPolicy
+$Execution = Get-ExecutionPolicy
+
 Start-Process -Path "C:\Program-Files\Google\Chrome\Application\Chrome.exe"
-Invoke-Webrequest 
+
