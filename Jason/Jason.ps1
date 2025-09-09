e66e1cd62c6a94721463d9bb461061dfad261446
Charles Dudley <tacoboycharles@gmail.com>
2025-09-09 08:14:40 -0500

    Delete Jason directory

--- Jason/Jason.ps1
+++
@@ -1,8 +0,0 @@
-Clear-Host -Force
-@echo off
-#Requires -RunAsAdministrator -Force
-Get-ExecutionPolicy
-$Execution = Get-ExecutionPolicy
-
-Start-Process -Path "C:\Program-Files\Google\Chrome\Application\Chrome.exe"
-
