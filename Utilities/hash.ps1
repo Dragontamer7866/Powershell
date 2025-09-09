9adb7b0dfb207e440e56d2d05122329bdbf55cc4
Charles Dudley <tacoboycharles@gmail.com>
2025-09-09 09:18:27 -0500

    Update hash.ps1

--- Utilities/hash.ps1
+++ Utilities/hash.ps1
@@ -1,1 +1,3 @@
 
+$hashPath = Write-Host -Prompt "What is the file path for the hash you need? "
+Get-FileHash -Path "$hashPath" -Algorithm SHA256 | Out-File "C:\Users\$env:USERNAME\Desktop\outputHash.txt"
